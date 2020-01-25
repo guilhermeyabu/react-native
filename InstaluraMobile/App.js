a4494947e80025f58416c449b183fb0d9d637635
Guilherme Yabu <guilhermeyabu@hotmail.com>
2020-01-25 18:54:05 -0300

    Create default list of people to add more items in the screen and change View to ScrollView

--- InstaluraMobile/App.js
+++ InstaluraMobile/App.js
@@ -7,20 +7,25 @@
  */
 
 import React from 'react';
-import { SafeAreaView, View, Text, Image, Dimensions } from 'react-native';
+import { SafeAreaView, View, Text, Image, Dimensions, ScrollView } from 'react-native';
 
 const deviceWidth = Dimensions.get('screen').width;
+
+const people = [{ id: 1, nome: 'Yabu' }, { id: 2, nome: 'Gabi' }, { id: 3, nome: 'Bazinga' }];
 
 export default function App() {
   return (
     <>
       <SafeAreaView>
-        <View>
-          <Text>Yabunito</Text>
-          <Image source={require('./assets/myself.jpg')}
-            style={{ width: deviceWidth, height: deviceWidth }}
-          />
-        </View>
+        <ScrollView>
+          {people.map(person =>
+            <View key={person.id}>
+              <Text>{person.nome}</Text>  
+              <Image source={require('./assets/myself.jpg')}
+                style={{ width: deviceWidth, height: deviceWidth }} />
+            </View>
+          )}
+        </ScrollView>
       </SafeAreaView>
     </>
   );
